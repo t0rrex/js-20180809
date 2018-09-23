29db744801f36b0eb6c73f91a3a7e1e1b2a51050
Prometheus <glebus24@mail.ru>
2018-09-23 20:38:48 +0300

    Adds server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+
+app.use(express.static(__dirname + '/'));
+
+
+var server = app.listen(3000, function () {
+
+
+    var port = server.address().port;
+
+
+    console.log('Your server is running at http://:%s', port);
+
+
+});
+
